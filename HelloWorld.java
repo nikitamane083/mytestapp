1312fef53f4948ed24ebfb7b9785e7dc494883ea
Nikita Mane <5739115-nikitamane083@users.noreply.gitlab.com>
2020-12-12 19:23:30 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main( String[] args){
+        System.out.println("Hello World");
+         System.out.println("Added new line");
+         int num=100;
+    }
+}
