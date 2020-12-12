683d610f9d80e1cd8895c48071a935f8a5347569
Nikita Mane <5739115-nikitamane083@users.noreply.gitlab.com>
2020-12-12 19:57:22 +0530

    added code

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,10 @@ public class HelloWorld{
     public static void main( String[] args){
         System.out.println("Hello World");
          System.out.println("Added new line");
+		 System.out.println("Added new line");
+      System.out.println("Added new line");
+System.out.println("Added new line");
+
          int num=100;
     }
 }
